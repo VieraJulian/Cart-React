7bf6dd40a83ae8b4fb44e9314e9db487b6587299
Julián Viera <julianviera2019@hotmail.com>
2023-03-12 19:12:31 -0300

    Julián - Arreglos en la vista

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ export const Header = ({ allProducts, setAllProducts, total, setTotal, countProd
 
     const onClearCart = () => {
         setTotal(0),
-        setCountProducts(0)
+            setCountProducts(0)
         setAllProducts([])
     }
 
@@ -52,25 +52,31 @@ export const Header = ({ allProducts, setAllProducts, total, setTotal, countProd
                                     {allProducts.map(product => (
                                         <div className="cart-product" key={product.id}>
                                             <div className="info-cart-product">
-                                                <span className="cantidad-producto-carrito">{product.quantity}</span>
+                                                <p className="cantidad-producto-carrito">{product.quantity}</p>
                                                 <p className="titulo-producto-carrito">{product.nameProduct}</p>
-                                                <span className="precio-producto-carrito">${product.price}</span>
+                                                <p className="precio-producto-carrito">${product.price}</p>
+                                                <div className="btnsQuantity">
+                                                    <button className="btnQuantityMore">+</button>
+                                                    <button className="btnQuantityLess">-</button>
+                                                </div>
                                             </div>
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                fill="none"
-                                                viewBox="0 0 24 24"
-                                                strokeWidth="1.5"
-                                                stroke="currentColor"
-                                                className="icon-close"
-                                                onClick={() => onDeleteProduct(product)}
-                                            >
-                                                <path 
-                                                    strokeLinecap="round"
-                                                    strokeLinejoin="round"
-                                                    d="M6 18L18 6M6 6l12 12"
-                                                />
-                                            </svg>
+                                            <div>
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    fill="none"
+                                                    viewBox="0 0 24 24"
+                                                    strokeWidth="1.5"
+                                                    stroke="currentColor"
+                                                    className="icon-close"
+                                                    onClick={() => onDeleteProduct(product)}
+                                                >
+                                                    <path
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        d="M6 18L18 6M6 6l12 12"
+                                                    />
+                                                </svg>
+                                            </div>
                                         </div>
                                     ))}
                                 </div>
